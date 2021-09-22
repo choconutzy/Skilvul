c37fee7b6fa0647c7a5cceb3b6d5c809a73663a0
choconutzy <78308985+choconutzy@users.noreply.github.com>
2021-09-22 14:47:00 +0700

    Create 04-capitalize-phrase.js

---
+++ codechallenge/data_type/04-capitalize-phrase.js
@@ -0,0 +1,4 @@
+function capitalize(kalimat){
+  return kalimat.charAt(0).toUpperCase() + kalimat.slice(1);
+}
+console.log(capitalize('javascript adalah sebuah bahasa pemrograman yang sangat powerful'));
