f514b6a69f9fb4dc6065db65218c526851f76e86
choconutzy <78308985+choconutzy@users.noreply.github.com>
2021-09-22 23:49:01 +0700

    Update 02-operator.js

--- 06-JavaScript-Dasar/02-operator.js
+++ 06-JavaScript-Dasar/02-operator.js
@@ -22,16 +22,18 @@ const sisaGaji = totalGaji - totalPengeluaran;
 /// Gunakan operator yang tepat untuk mengetahui apakah 2 variable dibawah ini memiliki nilai yang sama (tipe data tidak termasuk)
 const oneInString = "1";
 const oneInNumber = 1;
-const isSame;
+const isSame = (oneInNumber==oneInString);
+console.log(isSame)
 
 
 /// Soal - 03
 /// Gunakan operator yang tepat untuk mengetahui apakah studentA memiliki nilai yang lebih tinggi atau sama dengan studentB
 const studentA = 75;
 const studentB = 80;
-const isHigher;
+const isHigher = (studentA>studentB);
+console.log(isHigher);
 
 
 /// Soal - 04
 /// Gunakan operator yang tepat untuk membedakan 2 buah variable yang terdapat pada soal-02 berdasarkan value-nya (tipe data juga dihitung)
-const final = oneInString===oneInNumber;
+const final = (oneInString===oneInNumber);
