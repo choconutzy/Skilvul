a81de46740d896f9b91a751fab7d2fad0e245e04
choconutzy <78308985+choconutzy@users.noreply.github.com>
2021-09-22 22:52:15 +0700

    Update 04-loop.js

--- 06-JavaScript-Dasar/04-loop.js
+++ 06-JavaScript-Dasar/04-loop.js
@@ -20,10 +20,42 @@ console.log(result.toString())
 
 /// Soal - 02
 /// Kamu diminta untuk menemukan bilangan prima ke-50, temukan bilangan tersebut menggunakan while loop 
-
+let fiftiethPrime;
+let result = []
+let i = 2
+while(i<500){
+  let isPrime = true;
+  let j = 2;
+  while(j<i){
+    if(i%j==0){
+      isPrime = false;
+      break;
+    }
+    j++;
+  }
+  if(isPrime){
+    result.push(i)
+  }
+  i++;
+}
+fiftiethPrime = result[49] // ke 49 karena index dimulai dari nol
+console.log(fiftiethPrime)
 
 
 /// Soal - 03
 /// Kamu diminta untuk menemukan bilangan ganjil ke-50, temukan bilangan tersebut menggunakan do while loop
 let oddCounter = 0;
 let fiftiethOdd;
+let nth = 50; // input
+let iterations = 1;
+
+do{
+    const isOdd = iterations%2;
+    if(isOdd){
+        oddCounter++;
+        fiftiethOdd = iterations;
+    }
+    iterations++;
+} while(oddCounter<nth);
+
+console.log(fiftiethOdd)
