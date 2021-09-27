4ba14789fa3e39c90a2381f5b3b985e03c92f025
choconutzy <aprilianissa112@gmail.com>
2021-09-28 05:09:10 +0700

    update jawaban

---
+++ 07-JavaScript-Intermediet/01-array.js
@@ -0,0 +1,24 @@
+/// Soal - 01
+// const arr = [1, 2, 3];
+// const arr2 = [8, 1, 5, 7];
+// function reverse(array) {
+//     return array.reverse()
+// }
+
+// const newArr = reverse(arr);
+// const newArr2 = reverse(arr2);
+
+// console.log(arr, newArr);
+// console.log(arr2, newArr2);
+
+/// Soal - 02
+const arr1 = [1, 3, 4, 1, 2, 8];
+const arr2 = [5, 6, 7, 8, 1, 3];
+// sum = 0;
+function getAverage(y) { 
+
+ }
+
+console.log(getAverage(arr1))
+console.log(getAverage(arr2))
+
